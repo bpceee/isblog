cc5b05490d9a9b6e7188132a2bfeedc5386effdb
bpceee <bipengcheng@gmail.com>
2018-11-26 21:44:04 +0800

    lazy load comments

--- src/components/DumbPost.js
+++ src/components/DumbPost.js
@@ -1,33 +1,50 @@
 import React from 'react';
 import 'github-markdown-css/github-markdown.css'
-import { createMarkup } from '../utils';
+import { createMarkup, getLastElement } from '../utils';
 import styles from './DumbPost.module.css';
 import PostTags from "./PostTags";
 import PostDate from "./PostDate";
 
+
 class DumbPost extends React.Component {
 
   componentDidMount () {
-    const {post} = this.props;
-    if (!post.number) {
-      return;
+    document.title = this.props.post.title;
+  }  
+
+  componentWillUnmount() {
+    if (this.utteranceScript) {
+      this.utteranceScript.remove();
+      const utterances = document.querySelector(".utterances");
+      utterances && utterances.remove();
     }
-    document.title = post.title;
+  }
+
+  loadUtterance = () => {
     const main = document.querySelector("main");
     const script = document.createElement("script");
     script.src = "https://utteranc.es/client.js";
     script.setAttribute('repo', `${process.env.REACT_APP_USERNAME}/${process.env.REACT_APP_REPO}`);
-    script.setAttribute('issue-number', post.number);
+    script.setAttribute('issue-number', this.props.post.number);
     script.async = true;
     main.appendChild(script);
     this.utteranceScript = script;
-  }  
+  };
 
-  componentWillUnmount() {
-    this.utteranceScript.remove();
-    const utterances = document.querySelector(".utterances");
-    utterances && utterances.remove();
-  }
+  setBodyRef = ele => {
+    if (!ele) {
+      return;
+    }
+    const lastEle = getLastElement(ele);
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].intersectionRatio <= 0) {
+        return;
+      }
+      this.loadUtterance();
+      observer.unobserve(lastEle);
+    }, {threshold: 1});
+    observer.observe(lastEle);
+  };
 
   render() {
     const {post} = this.props;
@@ -47,7 +64,7 @@ class DumbPost extends React.Component {
             </p>
           </header>
         }
-        <div className="markdown-body" dangerouslySetInnerHTML={createMarkup(post.bodyHTML)}></div> 
+        <div className="markdown-body" ref={this.setBodyRef} dangerouslySetInnerHTML={createMarkup(post.bodyHTML)}></div> 
       </article>
     );
   }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,8 @@
 export function createMarkup(markup) {
   return {__html: markup};
 }
+
+export function getLastElement(ele) {
+  if (!ele || !ele.lastElementChild) return ele;
+  return getLastElement(ele.lastElementChild);
+}
